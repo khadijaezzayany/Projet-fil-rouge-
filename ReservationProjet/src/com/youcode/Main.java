ff41f3276c9e9ad578a47a342c7bc131816a6775
khadijaezzayany <khadijaezzayany8@gmail.com>
2021-07-12 01:54:47 +0100

    add working enviroment

---
+++ ReservationProjet/src/com/youcode/Main.java
@@ -0,0 +1,62 @@
+package com.youcode;
+
+import com.youcode.Dao.ReservationDAOImp;
+import com.youcode.Dao.RoleDAO;
+import com.youcode.Dao.RoleDAOImp;
+import com.youcode.Dao.StudentDAOImp;
+import com.youcode.Dao.StudentDAO;
+
+import com.youcode.Dao.UserDAO;
+import com.youcode.Dao.UserDAOImp;
+import com.youcode.Respository.ReservationRepository;
+import com.youcode.Respository.UserRepository;
+import com.youcode.entities.Reservation;
+import com.youcode.entities.Role;
+import com.youcode.entities.Student;
+import com.youcode.entities.TypeReser;
+import com.youcode.entities.User;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+//		StudentDAO studentdoa = new StudentDAOImp();
+//		Student student = new Student();
+//		studentdoa.updateStudent(8L,"lqdn", "qldj", "khadija@gmail.com", "123", 12121212);
+//		
+
+//		Role role = new Role();
+
+		//RoleDAO r = new RoleDAOImp();
+//		System.out.println(r.getRoleById(1L));
+
+		// User user = ;
+		// userdoa.addUser(new User("hhj", "hdh", "khadija@gmaail", "hgf", 762879,
+		// role));
+		 //Role r = new Role();
+//		userdoa.addRole(role);
+		// System.out.println(userdoa.getAllRole().get(0).getRoleName());
+//
+//		Student student = new Student(13L,"lqdn", "qldj", "Ali@gmail.com", "123", 12121212, r.getRoleById(3L));
+//		StudentDAOImp studentDao = new StudentDAOImp();
+//		studentDao.updateStudent(student);
+		
+		
+//		UserRepository user = new UserRepository();
+//		User us = user.getUserByEmail("Ali@gmail.com");
+//		System.out.println(us.getFirstName());
+		
+		
+//		ReservationDAOImp f = new ReservationDAOImp();
+//		System.out.println(f.listReservation());
+		
+		
+	
+//		UserDAOImp f = new UserDAOImp();
+//		f.removeUser(6L);
+		
+		
+		
+	}
+
+}
